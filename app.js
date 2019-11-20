e92850808fe7777a702593c3be51c0d06f2ee32c
Rajat <rpandey@mdsol.com>
2019-11-20 14:13:46 +0530

    Formatted

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const geoCode = require('./geocode')
 const weatherDetails = require('./weatherDetails')
 
+const address = 'Los Angeles'
  geoCode('Los Angeles', (error, data) => {
-   console.log(error)
   if(error === null) {
-  const latitude = data.latitude
-  const longitude = data.longitude
-  console.log('D', data)
-  weatherDetails(latitude, longitude, details => {
-   console.log('Received Currently>>>', details)
-  })
+   const latitude = data.latitude
+   const longitude = data.longitude
+    weatherDetails(latitude, longitude, details => {
+      console.log(`The weather in ${address} will be`, details.icon + ' with around '+ details.temperature + ' of temperature')
+    })
   } else {
     console.log(error)
   }
